f1ff95320c22e606c3b1c88bd0ac38957f8925b6
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2026-04-14 12:12:05 +0200

    feat: InMemFileRequester

---
+++ lib/src/main/kotlin/org/example/InMemFileRequester.kt
@@ -0,0 +1,13 @@
+package org.example
+
+internal class InMemFileRequester(
+    private val testData: ByteArray
+) : FileRequester<IntRange> {
+    override suspend fun getBodySize(): Int {
+        return testData.size
+    }
+
+    override suspend fun getChunk(id: IntRange): ByteArray {
+        return testData.slice(id).toByteArray()
+    }
+}
