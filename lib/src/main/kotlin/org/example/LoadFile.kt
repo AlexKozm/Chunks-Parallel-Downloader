c4842249b579f119f57c96087b5143351bc58172
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2026-04-14 11:56:30 +0200

    feat: chunk size could be specified on internal level

--- lib/src/main/kotlin/org/example/LoadFile.kt
+++ lib/src/main/kotlin/org/example/LoadFile.kt
@@ -1,32 +1,21 @@
 package org.example
 
 import io.ktor.client.HttpClient
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 
 suspend fun HttpClient.loadFile(url: String, numOfParallelRequests: Int): MutableList<ByteArray> {
-    val fileRequester = HttpFileRequester(this, url)
-    val bodyByteSize = fileRequester.getBodySize()
-    val chunkSize = bodyByteSize / numOfParallelRequests + if (bodyByteSize % numOfParallelRequests > 0) 1 else 0
-    val realNumOfParallelRequests = when (chunkSize) {
-        0 -> 1 // TODO: chunkSize == 0 only if bodyByteSize == 0
-        1 -> bodyByteSize
-        else -> numOfParallelRequests
-    }
-    val chunksStorage = RAMChunksStorage(realNumOfParallelRequests)
-    coroutineScope {
-        (0 ..< realNumOfParallelRequests).map { requestNumber ->
-            val startOffsetInclusive = requestNumber * chunkSize
-            val endOffsetExclusive =
-                if (requestNumber < realNumOfParallelRequests - 1) (requestNumber + 1) * chunkSize
-                else bodyByteSize
-
-            async {
-                val chunk = fileRequester.getChunk(startOffsetInclusive ..< endOffsetExclusive)
-                chunksStorage.saveChunk(requestNumber, chunk)
+    return loadFile(
+        fileRequester = HttpFileRequester(this, url),
+        chunkSizeProvider = { bodySize ->
+            bodySize / numOfParallelRequests + if (bodySize % numOfParallelRequests > 0) 1 else 0
+        },
+        chunksStorageProvider = { bodySize, chunkSize ->
+            val realNumOfParallelRequests = when (chunkSize) {
+                0 -> 1 // TODO: chunkSize == 0 only if bodyByteSize == 0
+                1 -> bodySize
+                else -> numOfParallelRequests
             }
-        }.awaitAll()
-    }
-    return chunksStorage.mergeChunks()
+            RAMChunksStorage(realNumOfParallelRequests)
+        },
+        numOfParallelRequests = numOfParallelRequests
+    )
 }
---
+++ lib/src/main/kotlin/org/example/LoadFileInternal.kt
@@ -0,0 +1,44 @@
+package org.example
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+
+// TODO: Consider using Long instead of Int, or even some generic type for ChunkId
+internal suspend fun <Result> loadFile(
+    fileRequester: FileRequester<IntRange>,
+    chunkSizeProvider: (bodySize: Int) -> Int,
+    chunksStorageProvider: (bodySize: Int, chunkSize: Int) -> ChunksStorage<Result, Int>,
+    numOfParallelRequests: Int
+): Result {
+    val bodySize = fileRequester.getBodySize()
+    val chunkSize = chunkSizeProvider(bodySize)
+    val chunksStorage = chunksStorageProvider(bodySize, chunkSize)
+    val iterator = IntRageIterator(chunkSize, bodySize)
+    val semaphore = Semaphore(numOfParallelRequests)
+    coroutineScope {
+        for ((index, chunkRange) in iterator.withIndex()) {
+            semaphore.acquire()
+            launch {
+                val chunk = fileRequester.getChunk(chunkRange)
+                chunksStorage.saveChunk(index, chunk)
+                semaphore.release()
+            }
+        }
+    }
+    return chunksStorage.mergeChunks()
+}
+
+private class IntRageIterator(
+    val chunkSize: Int,
+    val bodySize: Int
+) : Iterator<IntRange> {
+    var chunkCounter: IntRange = -1 ..< 0
+    override fun next(): IntRange {
+        val first = chunkCounter.last + 1
+        val lastExclusive = (first + chunkSize).coerceAtMost(bodySize)
+        chunkCounter = first ..< lastExclusive
+        return chunkCounter
+    }
+    override fun hasNext() = chunkCounter.last != bodySize - 1
+}
