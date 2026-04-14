a86d8808246c818740a1bd96caaa1aba164a23c7
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2026-04-14 16:40:37 +0200

    fix: add open options in FileChannel.open()

--- lib/src/main/kotlin/org/example/DiskChunksStorage.kt
+++ lib/src/main/kotlin/org/example/DiskChunksStorage.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.withContext
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption.*
 
 
 internal class DiskChunksStorage(
@@ -13,7 +14,7 @@ internal class DiskChunksStorage(
 ) : ChunksStorage<Path, Int> {
     override suspend fun saveChunk(id: Int, chunk: ByteArray) {
         withContext(Dispatchers.IO) {
-            FileChannel.open(path).use { channel ->
+            FileChannel.open(path, CREATE, WRITE).use { channel ->
                 channel.write(ByteBuffer.wrap(chunk), chunkSize * id.toLong())
             }
         }
